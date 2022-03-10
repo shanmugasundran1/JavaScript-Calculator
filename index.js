736ec659cd8b04d158e69203133cbde32514e3a2
SHANMUGASUNDRAN RAMACHANDRAN <86721748+shanmugasundran1@users.noreply.github.com>
2022-03-11 00:23:56 +0800

    Initial Push

---
+++ index.js
@@ -0,0 +1,35 @@
+let num1 = 8
+let num2 = 2
+
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+
+let sumEl = document.getElementById("sum-el")
+
+function add()
+{
+	let result = num1 + num2
+	sumEl.textContent = "Sum: "
+	sumEl.textContent += result
+}
+
+function sub()
+{
+	let result = num1 - num2
+	sumEl.textContent = "Sum: "
+	sumEl.textContent += result
+}
+
+function divide()
+{
+	let result = num1 / num2
+	sumEl.textContent = "Sum: "
+	sumEl.textContent += result
+}
+
+function mult()
+{
+	let result = num1 * num2
+	sumEl.textContent = "Sum: "
+	sumEl.textContent += result
+}
